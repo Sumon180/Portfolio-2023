89e93e2f392aa083222cebf7b1c0202dd31fd2b2
Sumon Hossain <sumonengr.96@gmail.com>
2023-07-13 15:14:28 +0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css"
+
+const About = () => {
+  return <div className={`${styles.about} text-4xl`}>About page</div>;
+};
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./page.module.css";
+
+const Contact = () => {
+  return <div className={`${Style.contact}`}>Contact</div>;
+};
+
+export default Contact;
